50244d0551655797363abf5e432f270e5459c4bf
Matt Anderson <matt.anderson.inf@gmail.com>
2020-01-19 21:18:59 -0500

    Added base html, css, and js files
    Added base html, css, and js files

---
+++ app.js
@@ -0,0 +1,79 @@
+/* app.js */
+
+// table container
+var animatedTable = document.querySelector("table.board");
+
+/*************************************************************
+ *  ON LOAD ANIMATION
+ *  When the page is first loaded, show three random points  
+ *  on the animated to demonstrate the pulse effect. 
+ * ***********************************************************/
+function onLoad(){
+  var rand1 = [Math.floor(Math.random()*24), Math.floor(Math.random()*32)];
+  var rand2 = [Math.floor(Math.random()*24), Math.floor(Math.random()*32)];
+  var rand3 = [Math.floor(Math.random()*24), Math.floor(Math.random()*32)];
+  pulsePeachColor(rand1[0],rand1[1],Math.floor(Math.random()*4)+1);
+  pulsePeachColor(rand2[0],rand2[1],Math.floor(Math.random()*4)+1);
+  pulsePeachColor(rand3[0],rand3[1],Math.floor(Math.random()*4)+1);
+}
+
+/*************************************************************
+ *  ANIMATED TABLE EVENT LISTENER
+ *  If the element pressed is a table cell then call the 
+ *  pulsePeach function. The pulseDepth is randomly set to an 
+ *  integer between 1 and 4.
+ * ***********************************************************/
+animatedTable.addEventListener("click", function(){
+    
+    // && !(event.target.classList.contains("animate-table-cell"))
+    if (event.target.tagName == "TD" ){
+        
+        pulsePeachColor(event.target.id.substring(0, event.target.id.indexOf("-")), event.target.id.substring(event.target.id.indexOf("-")+1, event.target.id.length), Math.floor(Math.random()*4)+1);
+    
+    }
+
+});
+
+/*************************************************************
+ *  PULSE PEACH FUNCTION
+ *  Given an x coordinate, y coordinate, and pulseDepth, add  
+ *  animate-table-cell to the table cell at those coordinates,
+ *  remove the animation class after 500ms, and call the pulse
+ *  function with a decremented pulse depth.
+ * ***********************************************************/
+function pulsePeachColor(rowNum, colNum, pulseDepth){
+    
+    if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum)){
+
+        document.getElementById(rowNum+"-"+colNum).classList.add("animate-table-cell");
+        
+        window.setTimeout(function() {
+            document.getElementById(rowNum+"-"+colNum).classList.remove('animate-table-cell');
+
+        },500);
+
+        window.setTimeout(function() {
+            pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
+            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
+            pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
+            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
+        },200); 
+        
+    }
+
+}
+
+/*************************************************************
+ *  VERIFY COORDINATES FUNCTION
+ *  Check to see if the given coordinates correspond to an 
+ *  existing element that has an id attribute with these
+ *  coordinates.
+ * ***********************************************************/
+function verifyCoordinates(rowNum, colNum){
+    if (document.getElementById(rowNum+"-"+colNum) != null && rowNum >= 0 && rowNum <= 24 && colNum >= 0 && colNum <= 31){
+        return true;
+    }
+    else{
+        return false;
+    }
+}

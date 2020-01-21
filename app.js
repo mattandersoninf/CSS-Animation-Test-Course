e8921a6da9cd49fd2664a5d2d7d4278a624dd2bc
Matt Anderson <matt.anderson.inf@gmail.com>
2020-01-20 23:20:08 -0500

    Revised if condition of pulse function
    Revised if condition of pulse function

--- app.js
+++ app.js
@@ -25,7 +25,6 @@ function onLoad(){
  * ***********************************************************/
 animatedTable.addEventListener("click", function(){
     
-    // && !(event.target.classList.contains("animate-table-cell"))
     if (event.target.tagName == "TD" ){
         
         pulsePeachColor(event.target.id.substring(0, event.target.id.indexOf("-")), event.target.id.substring(event.target.id.indexOf("-")+1, event.target.id.length), Math.floor(Math.random()*4)+1);
@@ -43,25 +42,25 @@ animatedTable.addEventListener("click", function(){
  * ***********************************************************/
 function pulsePeachColor(rowNum, colNum, pulseDepth){
     
-    if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum)){
-
-        document.getElementById(rowNum+"-"+colNum).classList.add("animate-table-cell");
-        
-        window.setTimeout(function() {
-            document.getElementById(rowNum+"-"+colNum).classList.remove('animate-table-cell');
-
-        },500);
-
-        window.setTimeout(function() {
-            pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
-            pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
-        },200); 
-        
+    if(pulseDepth > -1 && verifyCoordinates(rowNum, colNum) && (!(document.getElementById(rowNum+"-"+colNum).classList.contains("animate-table-cell")))){
+          
+      document.getElementById(rowNum+"-"+colNum).classList.add("animate-table-cell");
+  
+      window.setTimeout(function() {
+          document.getElementById(rowNum+"-"+colNum).classList.remove('animate-table-cell');
+  
+      },500);
+  
+      window.setTimeout(function() {
+          pulsePeachColor(parseInt(rowNum,10)+1,parseInt(colNum,10),pulseDepth-1);
+          pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)+1,pulseDepth-1);
+          pulsePeachColor(parseInt(rowNum,10)-1,parseInt(colNum,10),pulseDepth-1);
+          pulsePeachColor(parseInt(rowNum,10),parseInt(colNum,10)-1,pulseDepth-1); 
+      },200); 
+    
     }
-
-}
+  
+  }
 
 /*************************************************************
  *  VERIFY COORDINATES FUNCTION
